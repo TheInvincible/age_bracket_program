6a45f1f77b70edb9c630804993640fd71e39f3bb
TheInvincible <evewish@gmail.com>
2019-12-04 16:06:04 +0300

    The second challenge of week two with a function that filters vowels from a string

---
+++ challenge_two/vowels.js
@@ -0,0 +1,19 @@
+/*  Counting the vowels in a given string let string_ = 'zeestring'
+    let vowels = [a,e,i,o,u]
+    Function countVowels takes a string eg string_
+    returns an array of length 2 newArray = ['a', 'b']
+ */
+
+let string_ = 'excellent string';
+let vowels = [a,e,i,o,u];
+
+function countVowels(string) {
+  for (let v = 0; v < string_.length; v++) {
+    if (v = vowels) {
+      newArray = []
+      return newArray[v];
+      console.log(newArray);
+    }
+  }
+}
+countVowels(string_)
